7efdbd3509428ec3900896741b9b32b4642b8234
axlxlw <wangxiaolongme@gmail.com>
2025-07-23 04:12:33 +0800

    增加打点日志

--- src/BilibiliDownload/parser.py
+++ src/BilibiliDownload/parser.py
@@ -129,6 +129,7 @@ class BilibiliParser:
         resp.raise_for_status()
         html = resp.text
         html = html.replace('\n', '')
+        log.warning(f"响应长度:{len(html)}")
         log.debug(f"fetch_resp:  {html}")
         soup = BeautifulSoup(html, 'html.parser')
         script_tags = soup.find_all('script')
